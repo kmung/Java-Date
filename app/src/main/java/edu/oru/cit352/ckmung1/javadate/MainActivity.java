d9236efc2e60785d1d60097e04a45a3caf4fd6f0
Khawm Mung <johnmungtawng5@gmail.com>
2023-04-09 22:34:51 -0500

    initial commit

---
+++ app/src/main/java/edu/oru/cit352/ckmung1/javadate/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+Author: Khawm Mung
+Program: Java Date
+Description: Main activity for the app
+ */
+package edu.oru.cit352.ckmung1.javadate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
